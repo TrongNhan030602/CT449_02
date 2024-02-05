36508b031f799e3704d717a2d71a67989702858d
TrongNhan030602 <ntnhan030602@gmail.com>
2024-02-05 08:17:04 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.use(cors());
 
 app.use(express.json());
 
+
+const contactsRouter = require("./app/routes/contact.route");
+
+app.use("/api/contacts", contactsRouter);
+
+module.exports = app;
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to contact book application!" });
 });
